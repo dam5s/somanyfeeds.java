60729bc95cece9c4279dd8b5f09df6f363ddcdc6
Damien Le Berrigaud <damien@itsdamo.com>
2014-08-05 17:57:38 -0600

    Added some logging through slf4j.

--- src/main/java/com/somanyfeeds/HttpGateway.java
+++ src/main/java/com/somanyfeeds/HttpGateway.java
@@ -1,6 +1,7 @@
 package com.somanyfeeds;
 
 import com.google.api.client.http.*;
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import static com.somanyfeeds.Utils.readInputStream;
 
 @Component
 public class HttpGateway {
+    private final Logger logger = LoggerFactory.getLogger(HttpGateway.class);
     private final HttpRequestFactory requestFactory;
 
     @Autowired
@@ -18,11 +20,21 @@ public class HttpGateway {
     }
 
     public String get(String url) throws IOException {
+        logger.debug("Performing GET - {}", url);
         HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(url));
         HttpResponse response = request.execute();
 
         try {
-            return readInputStream(response.getContent());
+            String body = readInputStream(response.getContent());
+
+            if (response.getStatusCode() >= 400) {
+                logger.error("Received an error with status {}", response.getStatusCode());
+                logger.error("Body **********\n{}\n**********", body);
+            } else {
+                logger.debug("Received status {}", response.getStatusCode());
+            }
+
+            return body;
         } finally {
             response.disconnect();
         }
--- src/main/java/com/somanyfeeds/feeds/FeedUpdatesScheduler.java
+++ src/main/java/com/somanyfeeds/feeds/FeedUpdatesScheduler.java
@@ -1,14 +1,15 @@
 package com.somanyfeeds.feeds;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Component
 public class FeedUpdatesScheduler implements Runnable {
-    private final int delayBetweenRuns = 10;
+    private final Logger logger = LoggerFactory.getLogger(FeedUpdatesScheduler.class);
+    private final int delayBetweenRuns = 5 * 60;
     private final ScheduledExecutorService scheduledExecutorService;
     private final FeedUpdatesService feedUpdatesService;
 
@@ -22,13 +23,18 @@ public class FeedUpdatesScheduler implements Runnable {
         scheduleNextRun(0);
     }
 
-    private void scheduleNextRun(int minutes) {
-        scheduledExecutorService.schedule(this, minutes, TimeUnit.MINUTES);
+    private void scheduleNextRun(int seconds) {
+        scheduledExecutorService.schedule(this, seconds, TimeUnit.SECONDS);
     }
 
     @Override
     public void run() {
-        feedUpdatesService.updateAll();
-        scheduleNextRun(delayBetweenRuns);
+        try {
+            feedUpdatesService.updateAll();
+        } catch (Exception e) {
+            logger.error("There was an error while updating feeds", e);
+        } finally {
+            scheduleNextRun(delayBetweenRuns);
+        }
     }
 }
